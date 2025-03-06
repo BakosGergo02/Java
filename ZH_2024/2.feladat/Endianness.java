298f59efe2a5571398cc5bbc6c86dc05b73dd28f
Bakos Gergő <99805027+BakosGergo02@users.noreply.github.com>
2025-03-06 22:43:31 +0100

    Add files via upload

---
+++ ZH_2024/2.feladat/Endianness.java
@@ -0,0 +1,23 @@
+public class Endianness{
+
+    public String input;
+
+    public String reverseEndianness() {
+    
+        int len = input.length();
+         String temp = "";
+        
+
+        for (int i = 0; i < len/2;) {
+        }
+
+        return
+    }
+
+    @Override
+    public String toString() {
+        return "0x"+;
+    }
+    
+    
+}
